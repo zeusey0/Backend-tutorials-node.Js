4243912d421cce29380c61979e7fadae6b79a217
Bilal Usman <bilalusman1291@gmail.com>
2026-01-15 17:42:34 +0500

    ?? Node.Js Modules with Examples

--- Backend Tutorial Node.Js Modules/app.js
+++ Backend Tutorial Node.Js Modules/app.js
@@ -1,23 +1,13 @@
-const { createServer } = require("node:http");
-const fs = require("fs")
+const fs = require("fs");
 
-const port = 3000;
-const hostname = "127.0.0.1"
+// let text = fs.readFileSync("test.txt", "utf-8");
+// text = text.replace("Node", "javaScript");
+// fs.writeFileSync("style.txt", text);
+// console.log(text);
 
-const server = createServer((req, res) => {
-    res.statusCode = 200,
-        res.setHeader("content-Type", "text/html"),
-        res.end("<h1>Hello</h1>")
+
+fs.readFile("test.txt", "utf-8", (eror, data) => {
+    console.log(eror, data);
 });
 
-server.listen(port, hostname, () => {
-    console.log(`server in running: http://${hostname}:${port}/`);
-}); 
-
-
-let text = fs.readFileSync("test.txt", "utf-8");
-text = text.replace("Node", "Java");
-fs.writeFileSync("style.txt", text); 
-
- 
-console.log(text); 
+console.log("This is a masseges");
