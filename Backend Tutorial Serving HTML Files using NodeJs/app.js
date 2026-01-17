b3d3a89d2d9eb5dba2d2bc41d2a95fb2dd7d095e
Bilal Usman <bilalusman1291@gmail.com>
2026-01-18 03:59:56 +0500

    ?? update file

--- Backend Tutorial Serving HTML Files using NodeJs/app.js
+++ Backend Tutorial Serving HTML Files using NodeJs/app.js
@@ -1,15 +1,13 @@
 const http = require("http");
 const fs = require("fs");
-const fileRead = fs.readFileSync("text.html", "utf-8");
+
+const readFile = fs.readFileSync("test.txt", "utf-8");
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { "content-type": "text/html" });
-    res.end(fileRead);
+    res.writeHead(200, "content-Type", "text/html");
+    res.end(`<h1>${readFile}</h1>`);
 });
 
-server.listen(8000, "127.0.0.1", () => {
-    console.log("listening on port 80");
-});
-
-
-// git commit -m "?? Node.Js Modules with Examples"
+server.listen(3000, "127.0.0.1", () => {
+    console.log(`server in running: http://127.0.0.1:3000/`);
+})
